e5a75e89058cae16df7ffb909963e3cc1795dd56
karpent <karpent@gmail.com>
2017-09-17 18:45:09 +0200

    Initial FilterData component added

---
+++ source/SampleWebApp/SampleWebApp/ClientApp/components/FilterData.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { ApplicationState }  from '../store';
+import * as CounterStore from '../store/Counter';
+import * as WeatherForecasts from '../store/WeatherForecasts';
+
+type CounterProps =
+    CounterStore.CounterState
+    & typeof CounterStore.actionCreators
+    & RouteComponentProps<{}>;
+
+class FilterData extends React.Component<CounterProps, {}> {
+    public render() {
+        return <div>
+            <h1>Filter Data</h1>
+
+            <p>This is a simple example of a React component.</p>
+
+        </div>;
+    }
+}
+
+// Wire up the React component to the Redux store
+export default connect(
+    (state: ApplicationState) => state.counter, // Selects which state properties are merged into the component's props
+    CounterStore.actionCreators                 // Selects which action creators are merged into the component's props
+)(FilterData) as typeof FilterData;
